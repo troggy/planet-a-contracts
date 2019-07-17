fb0f09b94c518ce8f5930ce194314a04479824af
Johann Barbie <johannbarbie@me.com>
2019-07-17 19:06:36 +0200

    half way

--- contracts/Earth.sol
+++ contracts/Earth.sol
@@ -18,39 +18,71 @@ contract Earth {
   // air contract.
   address constant AIR_ADDR = 0x4561111111111111111111111111111111111456;
   
+  uint256 constant CO2_TO_GOELLARS_FACTOR = 1000;
+  uint256 constant LOW_TO_HIGH_FACTOR = 4;
+
+  function getGoellars(bool isDefectA, bool isDefectB, uint256 lowCO2) internal view returns (uint256) {
+    if (isDefectA) {
+      if (isDefectB) {
+        return (lowCO2 / CO2_TO_GOELLARS_FACTOR);
+      } else {
+        return (lowCO2 / CO2_TO_GOELLARS_FACTOR) * LOW_TO_HIGH_FACTOR;
+      }
+    } else {
+      if (isDefectB) {
+        return (lowCO2 / CO2_TO_GOELLARS_FACTOR) * LOW_TO_HIGH_FACTOR;
+      } else {
+        return (lowCO2 / CO2_TO_GOELLARS_FACTOR);
+      }
+    }
+  }
+
+  function getCo2B(bool isDefectB, uint256 lowCO2) internal view returns (uint256) {
+    if (isDefectB) {
+      return lowCO2 * LOW_TO_HIGH_FACTOR;
+    } else {
+      return lowCO2;
+    }
+  }
+
+  // (CO2mintedBefore - CO2mintedAfter) = co2Amount
+  // (CO2lockedBefore - CO2lockedAfter) > 0 ? collaborate : defect
+  // 
   function trade(
     uint256 passportA,
     bytes32 passDataAfter, 
     bytes memory sigA,
     uint256 passportB,
-    uint32 factorB,
+    bool isDefectB,
     address countryAaddr,
     address countryBaddr
   ) public {
     // calculate payout for A
     IERC1948 countryA = IERC1948(countryAaddr);
     bytes32 passDataBefore = countryA.readData(passportA);
-    uint256 factorA = uint256(passDataAfter) - uint256(passDataBefore);
-    // TODO calculate factor B
+    uint256 lowCO2 = uint256(uint32(uint256(passDataAfter)) - uint32(uint256(passDataBefore)));
+    bool isDefectA = false;
+
+    if ((uint256(passDataAfter) - uint256(passDataBefore) == lowCO2) {
+      // if CO2locked unchanged, then consider defect by player 1
+      lowCO2 = lowCO2 / LOW_TO_HIGH_FACTOR;
+      isDefectA = true;
+    }
 
     // pay out trade        
     IERC20 dai = IERC20(DAI);
-    // TODO: apply formula
-    dai.transfer(countryA.ownerOf(passportA), factorA);
+    dai.transfer(countryA.ownerOf(passportA), getGoellars(isDefectA, isDefectB, lowCO2));
     IERC1948 countryB = IERC1948(countryBaddr);
-    dai.transfer(countryB.ownerOf(passportB), uint256(factorB));
+    dai.transfer(countryB.ownerOf(passportB), getGoellars(isDefectA, isDefectB, lowCO2));
     
-    // TODO: apply formula
+    // update passports
     countryA.writeDataByReceipt(passportA, passDataAfter, sigA);
     bytes32 dataB = countryB.readData(passportB);
-    countryB.writeData(passportB, bytes32(uint256(dataB) + uint256(factorB)));
+    countryB.writeData(passportB, bytes32(uint256(dataB) + getCo2B(isDefectB, lowCO2)));
 
     // emit CO2
-    if (factorA > 100 || factorB > 100) {
-      IERC20 co2 = IERC20(CO2);
-      // TODO: apply formula
-      co2.transfer(AIR_ADDR, factorA + factorB);
-    }
+    IERC20 co2 = IERC20(CO2);
+    co2.transfer(AIR_ADDR, getCo2B(isDefectB, lowCO2) + uint256(uint32(uint256(passDataAfter)) - uint32(uint256(passDataBefore))));
   }
 
   // account used as game master.

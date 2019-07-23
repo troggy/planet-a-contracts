0fe5b22527a0e2041a04396872e618ed786c75e2
vrde <agranzot@gmail.com>
2019-07-23 12:20:22 +0200

    Add out dir to export script

--- scripts/createContracts.js
+++ scripts/createContracts.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const path = require("path");
 const { bufferToHex, ripemd160 } = require("ethereumjs-util");
 const Earth = require("../build/contracts/Earth");
 const Air = require("../build/contracts/Air");
+
+const outdir = process.argv[2] || ".";
+
+const airOutFile = path.join(outdir, "Air.json");
+const earthOutFile = path.join(outdir, "Earth.json");
 
 const testnetCO2 = "0xF64fFBC4A69631D327590f4151B79816a193a8c6";
 const testnetGOL = "0x1f89Fb2199220a350287B162B9D0A330A2D2eFAD";
@@ -32,19 +38,41 @@ airCode = replaceAll(
 );
 const airContractAddr = bufferToHex(ripemd160(airCode));
 
-fs.writeFileSync("Air.json", JSON.stringify({address: airContractAddr, code: airCode, abi: Air.abi}))
-console.log("Air.json exported")
+fs.writeFileSync(
+  airOutFile,
+  JSON.stringify({ address: airContractAddr, code: airCode, abi: Air.abi })
+);
+console.log("Air exported to", airOutFile);
 
 // only needed for testnet deployment
 let earthCode = Earth.deployedBytecode;
-earthCode = replaceAll(earthCode, "1231111111111111111111111111111111111123", testnetCO2);
-earthCode = replaceAll(earthCode, "2341111111111111111111111111111111111234", testnetGOL);
+earthCode = replaceAll(
+  earthCode,
+  "1231111111111111111111111111111111111123",
+  testnetCO2
+);
+earthCode = replaceAll(
+  earthCode,
+  "2341111111111111111111111111111111111234",
+  testnetGOL
+);
 earthCode = replaceAll(
   earthCode,
   "4561111111111111111111111111111111111456",
   airContractAddr
 );
-earthCode = replaceAll(earthCode, "5671111111111111111111111111111111111567", gameMaster);
+earthCode = replaceAll(
+  earthCode,
+  "5671111111111111111111111111111111111567",
+  gameMaster
+);
 const earthContractAddr = bufferToHex(ripemd160(earthCode));
-fs.writeFileSync("Earth.json", JSON.stringify({address: earthContractAddr, code: earthCode, abi: Earth.abi}))
-console.log("Earth.json exported")
+fs.writeFileSync(
+  "Earth.json",
+  JSON.stringify({
+    address: earthContractAddr,
+    code: earthCode,
+    abi: Earth.abi
+  })
+);
+console.log("Earth exported to", earthOutFile);
